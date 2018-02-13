380c7c3f13365051b3bd8f7fe648c3a30504493e
asifuzzamanrahat <asifuzzamanrahat@gmail.com>
2018-02-13 17:27:50 +0600

    problems

---
+++ problems/js/main.js
@@ -0,0 +1,25 @@
+(function($) { 
+
+    "use strict";
+    jQuery(document).ready(function($){
+     
+        $(".case-studies-item").owlCarousel({
+            items:3,
+            margin:30,
+            loop:true,
+            nav:false,
+            dots:true,
+        });
+        
+        $(".testimonial-item").owlCarousel({
+            items:1,
+            loop:true,
+            dots:true,
+            nav:false,
+        });
+        
+        jQuery(window).load(function(){
+            
+        });
+    });
+}(jQuery));

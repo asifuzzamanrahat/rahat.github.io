f7355e7d33c5b932e5a7463d6f13cb1e6cd3b9b7
asifuzzamanrahat <asifuzzamanrahat@gmail.com>
2018-02-15 15:45:41 +0600

    class3

---
+++ class3/js/custom.js
@@ -0,0 +1,40 @@
+   jQuery(document).ready(function ($) {
+        
+        
+       $('.owl_carousel').owlCarousel({
+        loop:true,
+        margin:30,
+        dots:true,   
+        nav:false,   
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:1
+            },
+            1000:{
+                items:3
+            }
+        }
+    })
+       
+       
+       $('.owl-carousel').owlCarousel({
+        loop:true,
+        nav:false,
+        dots:true,   
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:1
+            },
+            1000:{
+                items:1
+            }
+        }
+    })
+        
+    });

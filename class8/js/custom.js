2521fbb217099a3043f659401663c8a42520fdc9
asifuzzamanrahat <asifuzzamanrahat@gmail.com>
2018-02-27 10:28:42 +0600

    class8

---
+++ class8/js/custom.js
@@ -0,0 +1,76 @@
+   jQuery(document).ready(function ($) {
+        
+        
+       $('.owl_carousel').owlCarousel({
+        loop:true,
+        margin:30,
+        dots:true,   
+        nav:false,   
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:1
+            },
+            1000:{
+                items:3
+            }
+        }
+    })
+       
+       
+       
+       $('.owl-carousel').owlCarousel({
+        loop:true,
+        nav:false,
+        dots:true,   
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:1
+            },
+            1000:{
+                items:1
+            }
+        }
+    })
+       $('.logo-carousel').owlCarousel({
+        loop:true,
+        nav:false,
+        dots:false,   
+        responsive:{
+            0:{
+                items:2
+            },
+            600:{
+                items:4
+            },
+            1000:{
+                items:6
+            }
+        }
+    })
+       
+    
+       $('.homepage-slides').owlCarousel({
+        loop:true,    
+        dots:true,   
+        nav:true,
+        navText:["<i class='zmdi zmdi-arrow-left'></i>","<i class='zmdi zmdi-arrow-right'></i>"],   
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:1
+            },
+            1000:{
+                items:1
+            }
+        }
+    })   
+        
+    });

7fac2f799934c4db67220287e3118b94d77b135d
asifuzzamanrahat <36199178+asifuzzamanrahat@users.noreply.github.com>
2018-07-15 17:46:54 +0600

    class17

---
+++ class17/single page/js/custom.js
@@ -0,0 +1,56 @@
+   jQuery(document).ready(function ($) {
+        
+        
+       $('.owl_carousel').owlCarousel({
+        loop:true,
+        margin:30,
+        dots:false,   
+        nav:false,   
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:1
+            },
+            1000:{
+                items:5
+            }
+        }
+    })
+       
+       
+       $('.owl-carousel').owlCarousel({
+        loop:true,
+        nav:false,
+        dots:true,   
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:1
+            },
+            1000:{
+                items:1
+            }
+        }
+    })
+       $('.logo-carousel').owlCarousel({
+        loop:true,
+        nav:false,
+        dots:false,   
+        responsive:{
+            0:{
+                items:2
+            },
+            600:{
+                items:4
+            },
+            1000:{
+                items:6
+            }
+        }
+    })
+        
+    });
